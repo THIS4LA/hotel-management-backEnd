c6e2d97a6eef85ea76c882bad7d9e1737fd10dc2
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-02-14 10:37:45 +0530

    import body parser

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app= express();
+
+app.use(bodyParser.json());
 
 app.get("/",(req,res)=>{
     console.log("this is get request");
 })
 
+app.post("/",(req,res)=>{
+    console.log("this is post request");
+});
+
+app.put("/",(req,res)=>{
+    const name = req.body.name;
+    console.log(`Hi ${name}`);
+    res.json({
+        message: `Welcome, ${name}!`
+    })
+});
 app.listen(5000,(req,res)=>{
     console.log('Server is running on port 5000')
 });
