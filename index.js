9c412af475a9d56a54f35fe486ce41c7d289703e
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-04-09 08:47:53 +0530

    jwt middleware set

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 
 import userRouter from "./routes/usersRoute.js";
 import galleryItemRouter from "./routes/galleryItemRoute.js";
@@ -13,6 +14,23 @@ const app = express();
 app.use(bodyParser.json());
 
 const connectionString = process.env.MONGO_URL;
+
+app.use((req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
+  if (token != null) {
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+      if (decoded != null) {
+        req.user = decoded;
+        console.log(decoded);
+        next();
+      }
+    });
+  }
+  else {
+    next();
+  }
+});
 
 mongoose
   .connect(connectionString)
