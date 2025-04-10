cca7e1faacfa528020d972749cc6431e3b0e665b
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-04-10 21:20:50 +0530

    room model

---
+++ models/room.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const roomSchema = new mongoose.Schema(
+  {
+    roomId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    roomName: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: ["Standard", "Deluxe", "Luxury"],
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    features: {
+      type: [String],
+      default: [],
+    },
+    capacity: {
+      type: Number,
+      default: 1,
+    },
+    availability: {
+      type: Boolean,
+      default: true,
+    },
+    image: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdBi-6xt7I5uwI751cgv1S2JOTKohnNX7cfA&s",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Room = mongoose.model("Room", roomSchema);
+export default Room;
