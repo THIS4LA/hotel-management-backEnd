cb062d1d0008c727b9c4ab7716c635532a8d1ca6
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-08-24 13:33:25 +0530

    add routes for newly added controllers

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -1,9 +1,18 @@
 import express from "express";
-import {createBooking,getBookings} from "../controllers/bookingController.js";
+import {
+  createBooking,
+  getBookings,
+  deleteBooking,
+  updateBooking,
+  getAvailableRooms
+} from "../controllers/bookingController.js";
 
 const bookingRouter = express.Router();
 
-bookingRouter.post('/',createBooking);
-bookingRouter.get('/',getBookings);
+bookingRouter.post("/", createBooking);
+bookingRouter.get("/", getBookings);
+bookingRouter.get("/available", getAvailableRooms);
+bookingRouter.delete("/:id", deleteBooking);
+bookingRouter.put("/:id", updateBooking);
 
 export default bookingRouter;
