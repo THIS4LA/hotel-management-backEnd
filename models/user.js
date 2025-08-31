b919dd7e5ce055e6af7f18c2b72b17275678c403
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-08-31 22:37:53 +0530

    refactor user schema for improved readability and consistency

--- models/user.js
+++ models/user.js
@@ -1,56 +1,50 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 
-const userSchema = new mongoose.Schema(
-    {
-        email:{
-            type:String,
-            required:true,
-            unique:true
-        },
-        password:{
-            type:String,
-            required:true
-        },
-        firstName:{
-            type:String,
-            required:true
-        },
-        lastName:{
-            type:String,
-            required:true
-        },
-        type:{
-            type:String,
-            required:true,
-            default:"customer"
-        },
-        whatsapp:{
-            type:String,
-            required:true
-        },
-        phone :{
-            type:String,
-            required:true
-        },
-        image:{
-            type:String,
-            default:"https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
-        },
-        disabled:{
-            type:Boolean,
-            required:true,
-            default:false
-        },
-        emailVerified:{
-            type:Boolean,
-            required:true,
-            default:false
-        }
-        
-    }
-)
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    default: "customer",
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    default:
+      "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=",
+  },
+  disabled: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  emailVerified: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
 
 userSchema.plugin(mongoosePaginate);
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
